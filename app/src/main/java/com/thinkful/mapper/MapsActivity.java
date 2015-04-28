206b612f0b20a237204375718ec13db985c1da6e
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-04-27 21:54:49 -0700

    Initial commit

---
+++ app/src/main/java/com/thinkful/mapper/MapsActivity.java
@@ -0,0 +1,94 @@
+package com.thinkful.mapper;
+
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //Create Thinkful HQ Marker and show it
+        Marker thinkfulMarker = mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(40.72493, -73.996599))
+                        .title("Thinkful Headquarters!")
+                        .snippet("On a mission to reinvent education")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.thinkful))
+        );
+        thinkfulMarker.showInfoWindow();
+
+        //Move camera to Thinkful HQ
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(40.72493, -73.996599), 12));
+
+        //Set up a Handler to delay execution for 2 seconds
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //animate camera zoom
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(19),2000,null);
+            }
+        }, 2000);
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+}
